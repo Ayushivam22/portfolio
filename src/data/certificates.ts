1acd0f71ebc03f2a6c8b3f1b8a7d4fe23ead944c
Ayush Kr Shivam <106876019+Ayushivam22@users.noreply.github.com>
2025-07-08 12:36:40 +0530

    Update certificates.ts
    added google ux foundation certificate and added optimizations

--- src/data/certificates.ts
+++ src/data/certificates.ts
@@ -1,18 +1,22 @@
  export const certificates = [
         {
             label: "Certificate 1",
-            image: "https://res.cloudinary.com/den5u58in/image/upload/v1749191076/cert2_sp8eyx.png",
+            image: "https://res.cloudinary.com/den5u58in/image/upload/w_600,c_limit,f_auto,q_auto/v1749191076/cert2_sp8eyx.png",
         },
         {
             label: "Certificate 2",
-            image: "https://res.cloudinary.com/den5u58in/image/upload/v1749191075/cert4_dwsegb.jpg",
+            image: "https://res.cloudinary.com/den5u58in/image/upload/w_600,c_limit,f_auto,q_auto/v1749191075/cert4_dwsegb.jpg",
         },
         {
             label: "Certificate 3",
-            image: "https://res.cloudinary.com/den5u58in/image/upload/v1749191078/cert1_wvvu1b.png",
+            image: "https://res.cloudinary.com/den5u58in/image/upload/w_600,c_limit,f_auto,q_auto/v1749191078/cert1_wvvu1b.png",
         },
         {
             label: "Certificate 4",
-            image: "https://res.cloudinary.com/den5u58in/image/upload/v1749191076/cert3_jni1us.png",
+            image: "https://res.cloudinary.com/den5u58in/image/upload/w_600,c_limit,f_auto,q_auto/v1749191076/cert3_jni1us.png",
+        },
+        {
+            label: "Certificate 5",
+            image: "https://res.cloudinary.com/den5u58in/image/upload/w_600,c_limit,f_auto,q_auto/v1751958213/Screenshot_2025-07-08_123300_c5hxhm.png",
         },
     ];
