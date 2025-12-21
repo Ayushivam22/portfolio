38d11bb990bb5e2090bca0e9c794939813b261a2
AyushShivam <Ayushivam22@gmail.com>
2025-12-21 15:31:41 +0530

    fix: using dynamic links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { IoMenu } from "react-icons/io5";
+import { cv_link } from "../data/links";
 
 const Header = () => {
   const navElements = ["Home", "About", "Projects", "Contact"];
@@ -49,7 +50,7 @@ const Header = () => {
 
         {/* Download Button */}
         <div className="downloadcv-btn">
-          <a href="https://www.canva.com/design/DAGlo4LnZRg/aZK-qUgTcCE3agvS8pva0A/view?utm_content=DAGlo4LnZRg&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=hf09073a485" target="_blank">
+          <a href={cv_link} target="_blank">
           <button className="bg-orange-500 hover:bg-orange-600 text-white font-semibold py-2 px-2 text-xs rounded-md transition w-auto shadow-lg hover:shadow-xl focus:outline-none focus:ring-2 focus:ring-orange-400 focus:ring-opacity-50 hover:cursor-pointer">
             Download CV
           </button>
---
+++ src/data/links.ts
@@ -0,0 +1,3 @@
+const cv_link = "https://drive.google.com/file/d/179yKMOQVnhtOsPnsmsyO5EtdPc8WFOBr/view?usp=drive_link"
+
+export {cv_link}
